091da759ef88e2e2ee5e560f1d5e9679bae9cb94
TonicBox <gsec.tonicbox@protonmail.com>
2025-10-09 00:18:32 -0500

    # v1.3 - add option to disable loading and calling of RuneLites mouse hook DLL.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.tonic.launcher"
-version = "1.2"
+version = "1.3"
 
 repositories {
     mavenCentral()
--- src/main/java/com/tonic/launcher/ui/SplashScreen.java
+++ src/main/java/com/tonic/launcher/ui/SplashScreen.java
@@ -190,7 +190,7 @@ public class SplashScreen extends JWindow {
             g2d.setColor(new Color(150, 160, 180));
             Font subtitleFont = new Font("Arial", Font.PLAIN, 14);
             g2d.setFont(subtitleFont);
-            String subtitle = "Launcher v1.2";
+            String subtitle = "Launcher v1.3";
             FontMetrics subtitleMetrics = g2d.getFontMetrics(subtitleFont);
             int subtitleWidth = subtitleMetrics.stringWidth(subtitle);
             g2d.drawString(subtitle, centerX - subtitleWidth / 2, y);
