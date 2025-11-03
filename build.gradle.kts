70254e2ee4d94b8a8a970f624450f90fd3a6604f
TonicBox <gsec.tonicbox@protonmail.com>
2025-11-03 16:43:10 -0600

    # v2.1 support for running older runelite version while waiting for vita update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,6 @@ dependencies {
     implementation("org.apache.commons:commons-compress:1.26.0")
     implementation("org.apache.commons:commons-configuration2:2.10.1")
     implementation("commons-beanutils:commons-beanutils:1.11.0")
-    implementation("io.netty:netty-all:5.0.0.Alpha2")
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
     implementation(kotlin("stdlib-jdk8"))
